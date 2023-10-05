75f2e559ab1dd397e2deebc9acc459fc65d2b027
Mohamed_Ashraaaf <mohamed.ashraaf@gmail.com>
2023-10-05 11:34:16 +0000

    Basic annotations

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-"""
-    Return the floor of a float as an integer
-"""
+"""Basic annotations concat"""
 
 import math
 
+
 def floor(n: float) -> int:
-    """Return the floor of a float as an integer."""
+    """Returns the floor of the float"""
     return math.floor(n)
