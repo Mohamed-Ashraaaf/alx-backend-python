266abcc58c72aa14e33c7f3c7c03ae9c4748ea41
Mohamed_Ashraaaf <mohamed.ashraaf@gmail.com>
2023-10-09 17:46:39 +0000

    Async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
-"""Execute multiple coroutines at the same time with async"""
+"""
+Asynchronous coroutine that spawns multiple instances of wait_random.
+"""
 
 import asyncio
+from typing import List
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
-    """Return list of all delays in ascending order"""
-    delays = [asyncio.create_task(wait_random(max_delay))for _ in range(n)]
-    return [await delay for delay in asyncio.as_completed(delays)]
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawn wait_random n times with the specified max_delay
+    Args:
+        n (int): Number of times to spawn wait_random
+        max_delay (int): Maximum delay in seconds
+    Returns:
+        List[float]: List of delays in ascending order
+    """
+    delays = []
+    for _ in range(n):
+        delays.append(wait_random(max_delay))
+    gathered_delays = await asyncio.gather(*delays)
+    return sorted(gathered_delays)
