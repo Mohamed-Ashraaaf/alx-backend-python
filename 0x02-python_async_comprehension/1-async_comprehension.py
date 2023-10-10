45c1becaad30410cce757ab20940484f74715de7
Mohamed_Ashraaaf <mohamed.ashraaf@gmail.com>
2023-10-10 17:55:43 +0000

    Async again

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,24 +1,14 @@
 #!/usr/bin/env python3
-"""
-Async comprehension that collect random numbers.
-"""
+"""Async Comprehensions"""
 
 import asyncio
-from typing import List
+import random
+from typing import AsyncGenerator
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[float]:
-    """
-    Coroutine that collect rand numbers using async comprehension
-    """
-    return [i async for i in async_generator()]
-
-
-async def main():
-    print(await async_comprehension())
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+async def async_comprehension() -> AsyncGenerator[float, None]:
+    """Async Comprehensions"""
+    async for i in async_generator():
+        yield i
