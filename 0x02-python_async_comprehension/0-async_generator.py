d637a837b9a36fb701a2e323b053bc83cf3090fd
Mohamed_Ashraaaf <mohamed.ashraaf@gmail.com>
2023-10-10 17:41:09 +0000

    Async again

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,23 +5,11 @@ Async generator that yields random numbers
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
-    """
-    Coroutine that yields random numbers between 0 and 10.
-    """
+async def async_generator() -> Generator[float, None, None]:
+    """ Async Generator """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
-
-
-async def print_yielded_values():
-    result = []
-    async for i in async_generator():
-        result.append(i)
-    print(result)
-
-
-if __name__ == "__main__":
-    asyncio.run(print_yielded_values())
