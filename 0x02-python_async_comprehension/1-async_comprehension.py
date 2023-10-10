e5b6b9a4d0c41b0ce0dbf21d3518cc8bb7f01e5f
Mohamed_Ashraaaf <mohamed.ashraaf@gmail.com>
2023-10-10 17:43:49 +0000

    Async again

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Async comprehension that collects 10 random numbers.
+"""
+
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Coroutine that collects 10 random numbers using async comprehension.
+    """
+    return [i async for i in async_generator()]
+
+
+async def main():
+    print(await async_comprehension())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
